13717dbf4c25a07520268f993522fb16772ddbb0
m.arslangiraev <m.arslangiraev@iblschool.ru>
2022-07-15 01:09:37 +0300

    edits js files eslint

--- src/colors.js
+++ src/colors.js
@@ -1,27 +1,29 @@
-export const colors = [
-    {
-        backgroundColor: 'rgb(241,244,246, 0.3)',
-        color: '#F1F4F6',
-        id: 1,
-    },
-    {
-        backgroundColor: 'rgb(246, 215, 167, 0.7)',
-        color: '#E7BE7F',
-        id: 2,
-    },
-    {
-        backgroundColor: 'rgb(84, 160, 255, 0.1)',
-        color: '#54A0FF',
-        id: 3,
-    },
-    {
-        backgroundColor: 'rgb(214, 69, 65, 0.15)',
-        color: '#D64541',
-        id: 4,
-    },
-    {
-        backgroundColor: 'rgb(39, 174, 96, 0.15)',
-        color: '#27AE60',
-        id: 5,
-    },
-]
+const colors = [
+  {
+    backgroundColor: 'rgb(241,244,246, 0.3)',
+    color: '#F1F4F6',
+    id: 1,
+  },
+  {
+    backgroundColor: 'rgb(246, 215, 167, 0.7)',
+    color: '#E7BE7F',
+    id: 2,
+  },
+  {
+    backgroundColor: 'rgb(84, 160, 255, 0.1)',
+    color: '#54A0FF',
+    id: 3,
+  },
+  {
+    backgroundColor: 'rgb(214, 69, 65, 0.15)',
+    color: '#D64541',
+    id: 4,
+  },
+  {
+    backgroundColor: 'rgb(39, 174, 96, 0.15)',
+    color: '#27AE60',
+    id: 5,
+  },
+];
+
+export default colors;
